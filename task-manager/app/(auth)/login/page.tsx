75259260825115d4fe7afc452d0e240d7044e573
Murvyn <marvin.asamoah.123@gmail.com>
2023-12-07 17:25:43 +0000

    buid auth pages

---
+++ task-manager/app/(auth)/login/page.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { ZodType, z } from "zod";
+
+interface FormData {
+  email: string
+  password: string
+}
+
+
+const Login = () => {
+  const schema: ZodType<FormData> = z.object({
+    email: z.string().email(),
+    password: z.string().nonempty("Password is required"),
+  })
+
+  const {register, handleSubmit, formState:{errors}} = useForm<FormData>({resolver: zodResolver(schema)})
+
+  const submitHandler = (form: FormData) => {
+    console.log(form)
+  }
+  return (
+    <>
+      <div className="justify-center items-center w-100 h-[100vh] flex ">
+        <div className="items-center flex flex-col">
+          <h1 className="mb-3 text-3xl font-bold">Login</h1>
+          <form className="w-96 m-8 space-y-4" onSubmit={handleSubmit(submitHandler)}>
+            <div className="mb-3">
+              <input
+              {...register('email', {required: true})}
+                className="input input-bordered w-full "
+                type="email"
+                name="email"
+                id="email"
+                placeholder="Email"
+              />
+              {errors.email && <span className="text-error text-sm">{errors.email.message}</span>}
+            </div>
+            <div className="mb-3">
+              <input
+              {...register('password', {required: true})}
+                className="input input-bordered w-full "
+                type="text"
+                name="password"
+                id="password"
+                placeholder="Password"
+              />
+              {errors.password && <span className="text-error text-sm">{errors.password.message}</span>}
+            </div>
+            <button className="btn btn-primary w-full" type="submit">
+              Login
+            </button>
+          </form>
+          <div>
+            <p>
+              Need an account?{" "}
+              <Link
+                href="/sign-up"
+                className="link link-primary hover:cursor-pointer"
+              >
+                sign up
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Login;
