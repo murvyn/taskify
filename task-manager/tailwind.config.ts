86428dd5e01c27c4232d7d5d52eb6725a2ccf962
Murvyn <marvin.asamoah.123@gmail.com>
2023-12-26 14:02:22 +0000

    fix theme

--- task-manager/tailwind.config.ts
+++ task-manager/tailwind.config.ts
@@ -17,7 +17,7 @@ const config: Config = {
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {
-    themes: ["light", "dark", "cupcake", "winter", "halloween", "bumblebee"],
+    themes: ["halloween"],
   },
 }
 export default config
