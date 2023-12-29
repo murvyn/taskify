a8822454b48a0fac23ff57fbd1916eeb696141c7
Murvyn <marvin.asamoah.123@gmail.com>
2023-12-29 06:01:01 +0000

    change loading background

--- task-manager/app/loading.tsx
+++ task-manager/app/loading.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const loading = () => {
   return (
-    <div className="h-[100vh] w-full z-50 left-0 top-0 bg-base-300 flex">
+    <div className="h-[100vh] w-full z-50 left-0 top-0 flex">
       <div className=" h-full w-full justify-center items-center flex">
         <span className="loading loading-dots loading-lg"></span>
       </div>
