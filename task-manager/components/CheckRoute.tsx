68930753ef4b9c9aee69f32aac67be15369647b9
Murvyn <marvin.asamoah.123@gmail.com>
2024-01-08 22:33:27 +0000

    bug

--- task-manager/components/CheckRoute.tsx
+++ task-manager/components/CheckRoute.tsx
@@ -15,7 +15,7 @@ const CheckRoute = ({children}: {children : ReactNode}) => {
   return (
     <div className='flex gap-2'>
     <SideBar />
-    <div className="sm:card w-full h-[96vh] sm:bg-base-300 sm:border sm:border-stone-700 sm:shadow-2xl p-5 overflow-hidden sm:hover:overflow-y-scroll">
+    <div className="sm:card w-full h-[96vh] sm:bg-base-300 sm:border sm:border-stone-700 sm:shadow-2xl p-5 sm:overflow-hidden sm:hover:overflow-y-scroll">
     {children}
     </div>
     </div>
