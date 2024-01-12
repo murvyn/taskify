88cdfda9c58e6e3d7f2752a659bfadbf58b53af9
Murvyn <marvin.asamoah.123@gmail.com>
2024-01-12 10:27:33 +0000

    fix

--- task-manager/middleware.ts
+++ task-manager/middleware.ts
@@ -1,10 +1,10 @@
 // export { default } from "next-auth/middleware";
 
 import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 
-export async function middleware(req: Request){
+export async function middleware(req: NextRequest){
     const token = await getToken({req})
     if(token) return NextResponse.next()
     return NextResponse.redirect(new URL("/auth/login", req.url))
