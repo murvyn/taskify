c631dc10eddf2cecd6485b8624047b49a15dc5fd
Murvyn <marvin.asamoah.123@gmail.com>
2024-01-13 21:43:03 +0000

    fix width

--- task-manager/app/dashboard/completed/page.tsx
+++ task-manager/app/dashboard/completed/page.tsx
@@ -47,7 +47,7 @@ const CompletedBox = () => {
       <div>
         <h2 className="card-title mb-5 w-auto flex flex-col items-start ">
           Completed
-          <span className=" w-1/12 h-1 bg-primary"></span>
+          <span className="max-sm:w-1/12 w-10 h-1 bg-primary"></span>
         </h2>
         <Suspense fallback={<Loading />}>
           <TaskCard tasks={completedTasks} />
