9f13397a8a3b609e4aea46665719338c344d0667
Murvyn <marvin.asamoah.123@gmail.com>
2024-01-13 01:45:46 +0000

    notification

--- task-manager/contexts/taskContext.tsx
+++ task-manager/contexts/taskContext.tsx
@@ -1,6 +1,7 @@
 "use client";
+import { isSameTime } from "@/helpers";
 import { ChildrenProps, TaskContextProps, TaskProps } from "@/types";
-import { createContext, useState } from "react";
+import { createContext, useCallback, useEffect, useState } from "react";
 
 export const TaskContext = createContext<TaskContextProps>({
   tasks: [],
@@ -21,6 +22,37 @@ export const TaskProvider = ({ children }: ChildrenProps) => {
   const [completedCount, setCompletedCount] = useState(0);
   const [allTaskCount, setAllTaskCount] = useState(0);
   const [todayCount, setTodayCount] = useState(0);
+  const currentDate = new Date()
+
+  const taskReady = tasks?.filter((task) => {
+    return isSameTime(new Date(task.dateTime), currentDate);
+  });
+
+  // const sendNotification = () => {
+  //   if(window.Notification && Notification.permission === 'granted'){
+  //     new Notification("Hello",{
+  //       body: "This boss"
+  //     })
+  //   }
+  // }
+
+  // const requestNotification = useCallback(() => {
+  //   if(window.Notification){
+  //     Notification.requestPermission().then((result)=>{
+  //       if(result === "granted"){
+  //         console.log('User granted notification permission');
+  //         sendNotification();
+  //       }
+  //     })
+  //   }
+  // }, [])
+
+  // useEffect(() => {
+  //   if(window.Notification){
+  //     requestNotification();
+  //   }
+  // }, [requestNotification])
+
   const value: TaskContextProps = {
     setTasks,
     tasks,
