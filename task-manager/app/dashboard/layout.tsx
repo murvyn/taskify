4b7dcdf907801279570706e5f0fd5846d2e6f9e9
Murvyn <marvin.asamoah.123@gmail.com>
2024-01-16 13:48:05 +0000

    style

--- task-manager/app/dashboard/layout.tsx
+++ task-manager/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default function DashBoardLayout({
   return (
     <section className="flex gap-2 p-5">
       <SideBar />
-      <div className="sm:card w-full h-[96vh] sm:bg-base-300 sm:border sm:border-stone-700 sm:shadow-2xl p-5 sm:overflow-hidden sm:hover:overflow-y-scroll">
+      <div className="sm:card w-full h-[96vh] sm:bg-base-300 sm:border sm:border-stone-700 sm:shadow-2xl p-5 sm:overflow-hidden sm:overflow-y-scroll">
         {children}
       </div>
     </section>
